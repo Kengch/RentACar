fda2cd892092519570a65dcee7bde55dc5007af6
Kengch <88747640+Kengch@users.noreply.github.com>
2022-07-29 15:53:34 -0600

    Add files via upload

---
+++ src/rent_a_car/Rent_a_car.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rent_a_car;
+
+/**
+ *
+ * @author Keng
+ */
+public class Rent_a_car {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
